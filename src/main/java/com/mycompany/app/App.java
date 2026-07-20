2ed1c42259861d0885e4c4246bce40a8ac2ac3e3
sanduw001 <sanduniwelege04@gmail.com>
2026-07-20 16:48:00 +0530

    Initial Maven Hello World project

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,18 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args) {
+
+    String appName = System.getProperty("app.name");
+    String appVersion = System.getProperty("app.version");
+
+    System.out.println("App Name: " + appName);
+    System.out.println("App Version: " + appVersion);
+
+}
+}
